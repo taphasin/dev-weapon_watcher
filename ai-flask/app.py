ae24347ec8a9b80204372b87d1673ca830004635
tatanoi <ta.phasin@gmail.com>
2025-12-10 15:24:27 +0700

    work_rtsp

--- ai-flask/app.py
+++ ai-flask/app.py
@@ -21,7 +21,7 @@ def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
 def generate():
-    rtsp_url = "rtsp://admin:Admin.1234@192.168.1.64:554/Streaming/Channels/102"
+    rtsp_url = "rtsp://admin2:admin234@161.246.5.47:554"
     '''rtsp://rtspstream:ZqniBPd7ILatymFgJcK9W@zephyr.rtsp.stream/movie'''
     cap = None
 
